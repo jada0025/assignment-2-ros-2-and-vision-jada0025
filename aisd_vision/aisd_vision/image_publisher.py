f5012601ae382eba72773148ff3835cc7c344615
jada0025 <jada0025@algonquinlive.com>
2024-11-17 18:20:23 -0500

    Create image_publisher.py

---
+++ aisd_vision/aisd_vision/image_publisher.py
@@ -0,0 +1,46 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+
+from std_msgs.msg import String
+from cv_bridge import CvBridge
+import cv2
+
+
+class ImagePublisher(Node):
+
+    def __init__(self):
+        super().__init__('image_publisher')
+        self.publisher_ = self.create_publisher(String, 'video_frames', 10)
+        timer_period = 0.2  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.cap = cv2.VideoCapture(0)
+        self.br = CvBridge()
+
+
+    def timer_callback(self):
+        msg = Image()
+        msg.data = 'Hello World: %d'
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        ret, frame = self.cap.read()
+        if ret == True:
+          self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    image_publisher = ImagePublisher()
+
+    rclpy.spin(image_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    image_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
